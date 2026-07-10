ffee9aad00be25c6551393ae9839ec5aa4f28e95
mascari16 <mascari16@yahoo.com>
2026-07-10 13:50:14 -0400

    commit

--- js/flatOval.js
+++ js/flatOval.js
@@ -1,46 +1,6 @@
 // =====================================================
-// DUCT OPTIONS GENERATOR
+// FLAT OVAL DUCT OPTIONS GENERATOR
 // =====================================================
-
-// Rectangle options
-function getRectangleOptions(requiredArea, targetVelocity, cfm) {
-
-    const options = [];
-
-    for (let width = 8; width <= 96; width += 2) {
-
-        for (let height = 8; height <= 60; height += 2) {
-
-            const area = (width * height) / 144;
-
-            const velocity = cfm / area;
-
-            const diff = Math.abs(area - requiredArea) / requiredArea;
-
-            // Ignore anything too far away
-            if (diff > 0.12)
-                continue;
-
-            options.push({
-
-                width,
-                height,
-                area,
-                velocity,
-                diff
-
-            });
-
-        }
-
-    }
-
-    options.sort((a, b) => a.diff - b.diff);
-
-    return options.slice(0, 5);
-
-}
-
 
 // Flat Oval options
 function getFlatOvalOptions(requiredArea, targetVelocity, cfm) {
